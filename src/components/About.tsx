c1d3aea1ba7d8013a954788bb94de5aed93ce195
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-04-28 10:35:40 -0600

    feat: added Slider component into About section, to show technologies

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,11 @@
 import "@/styles/about.css";
 import { robotoSlab } from "@/vendor/fonts";
 import Image from "next/image";
+import Slider from "./Slider";
 
 export default function About() {
   return (
-    <section className="about flex">
+    <section className="about flex px-[104px] py-[80px]">
       <Image
         src="/about.jpg"
         width={464}
@@ -12,18 +13,21 @@ export default function About() {
         alt="Profile picture"
         className="about__photo rounded-full"
       />
-      <div className="about__info flex flex-col w-[600px]">
+      <div className="about__info flex flex-col w-[600px] pt-[50px]">
         <h3 className={`about__title ${robotoSlab.className}`}>
           Acerca del autor
         </h3>
-        <p className="about__description">
-          Este bloque describe al autor del proyecto. Aquí debe indicar tu
-          nombre, a qué te dedicas y qué tecnologías de desarrollo conoces.
-          <br />
-          <br />
-          También puedes hablar de tu experiencia con Practicum, de lo que
-          aprendiste allí y de cómo puedes ayudar a los clientes potenciales.
-        </p>
+        <div className="about__description">
+          <p>
+            Este bloque describe al autor del proyecto. Aquí debe indicar tu
+            nombre, a qué te dedicas y qué tecnologías de desarrollo conoces.
+          </p>
+          <Slider />
+          <p>
+            También puedes hablar de tu experiencia con Practicum, de lo que
+            aprendiste allí y de cómo puedes ayudar a los clientes potenciales.
+          </p>
+        </div>
       </div>
     </section>
   );
