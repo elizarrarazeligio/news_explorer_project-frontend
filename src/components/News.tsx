b7230f1a6ffe1fb1b9fe64822f1cc68efa64c0e7
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-04-30 06:44:13 -0600

    refactor/style: changed loader renderizing in News component and changed buttton margins as well

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -17,21 +17,26 @@ export default function News(props: NewsProps) {
   const { title, children, news, loader } = props;
 
   return (
-    <section className="news-cards flex flex-col px-[104px] py-[65px]">
-      <h3
-        className={`news-cards__title ${
-          robotoSlab.className
-        } mt-[15px] mb-[65px] ${title ? "" : "hidden"}`}
-      >
-        {title}
-      </h3>
-      <div className="news-cards__container grid">
-        {/* Se pasan los props de cada item con el operador spread (...) */}
-        {news && news.map((item) => <NewsItem key={item.id} {...item} />)}
-      </div>
+    <>
+      {loader ? (
+        <Preloader />
+      ) : (
+        <section className="news-cards flex flex-col px-[104px] py-[65px]">
+          <h3
+            className={`news-cards__title ${
+              robotoSlab.className
+            } mt-[15px] mb-[65px] ${title ? "" : "hidden"}`}
+          >
+            {title}
+          </h3>
+          <div className="news-cards__container grid">
+            {/* Se pasan los props de cada item con el operador spread (...) */}
+            {news && news.map((item) => <NewsItem key={item.id} {...item} />)}
+          </div>
 
-      {loader && <Preloader />}
-      {children}
-    </section>
+          {children}
+        </section>
+      )}
+    </>
   );
 }
