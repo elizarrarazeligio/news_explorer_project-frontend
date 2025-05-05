e14694056c68ebf1e7829aa5b35829347c95a9a5
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-05-04 21:08:17 -0600

    fix/docs: included a env example file to guide users on how to include their own env file, as well as changed the baseURL on the NewsAPI class

--- src/utils/NewsApi.ts
+++ src/utils/NewsApi.ts
@@ -1,26 +1,39 @@
 import Api from "./Api";
 
 class NewsApi extends Api {
-  constructor({ baseUrl, headers }: { baseUrl: string; headers: {} }) {
+  _fromDate: string;
+  _toDate: string;
+
+  constructor(
+    { baseUrl, headers }: { baseUrl: string; headers: {} },
+    fromDate: string,
+    toDate: string
+  ) {
     super({ baseUrl, headers });
+    this._fromDate = fromDate;
+    this._toDate = toDate;
   }
 
   searchNews(query: string): Promise<Object> {
-    return super._makeRequest(`&q=${query}`);
+    return super._makeRequest(
+      `/everything?from=${this._fromDate}&to=${this._toDate}&sortBy=publishedAt&language=es&q=${query}`
+    );
   }
 }
 
 // Obtener fechas de búsqueda de noticias (desde - hasta)
-let currentDate = new Date();
+const currentDate = new Date();
 const toDate = currentDate.toISOString();
 currentDate.setDate(currentDate.getDate() - 7);
 const fromDate = currentDate.toISOString();
 
-console.log(toDate, fromDate);
-
-export const newsApi = new NewsApi({
-  baseUrl: `https://newsapi.org/v2/everything?from=${fromDate}&to=${toDate}&sortBy=publishedAt&language=es`,
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_API_KEY}`,
+export const newsApi = new NewsApi(
+  {
+    baseUrl: "https://newsapi.org/v2",
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_API_KEY}`,
+    },
   },
-});
+  fromDate,
+  toDate
+);
